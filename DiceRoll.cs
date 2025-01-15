6c6663e2cf258a3b9e51870737275baa411d706f
koltonsg <153117250+koltonsg@users.noreply.github.com>
2025-01-15 11:30:03 -0700

    Add project files.

---
+++ DiceRoll.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace Mission2
+{
+    internal class DiceRoll
+    {
+        Random random = new Random();
+        private int[] scores = new int[11];
+        
+        public int[] Roll(int numRolls)
+        {
+            for (int i = 0; i < numRolls; i++)
+            {
+                int rollValue1 = random.Next(1, 7);
+                int rollValue2 = random.Next(1, 7);
+
+                if (rollValue1 + rollValue2 == 2)
+                {
+                    scores[0] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 3)
+                {
+                    scores[1] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 4)
+                {
+                    scores[2] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 5)
+                {
+                    scores[3] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 6)
+                {
+                    scores[4] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 7)
+                {
+                    scores[5] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 8)
+                {
+                    scores[6] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 9)
+                {
+                    scores[7] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 10)
+                {
+                    scores[8] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 11)
+                {
+                    scores[9] += 1;
+                }
+                else if (rollValue1 + rollValue2 == 12)
+                {
+                    scores[10] += 1;
+                }                
+            }
+            return scores;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Mission2;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Program
+{
+    private static void Main()
+    {
+        DiceRoll dr = new DiceRoll();
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\r\nHow many dice rolls would you like to simulate?: ");
+        string input = System.Console.ReadLine();
+
+        if (int.TryParse(input, out int numRolls))
+        {
+            System.Console.WriteLine($"You entered {numRolls} rolls.");
+            int[] results = dr.Roll(numRolls); 
+
+            for (int i = 0; i < results.Length; i++) 
+            {
+                if (results[i] > 0)
+                {
+                    double percentage = ((double)results[i]/numRolls)*100;
+                    double rounded = Math.Round((double)results[i]);
+
+                    Console.Write($"{i + 2}: ");
+
+                    for (int j = 0; percentage > j; j++)
+                    {
+                        Console.Write("*");
+                    }
+                Console.WriteLine();
+                }
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
+        System.Console.WriteLine("\nThanks for rolling the dice! See ya!");
+    }
+}
